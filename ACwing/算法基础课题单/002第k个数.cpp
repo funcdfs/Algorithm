282fcf2bf63590898453f72d229de3d772169197
fengwei2002 <konng_0120@qq.com>
2021-06-30 21:52:19 +0800

    第k个数 归并

---
+++ ACwing/算法基础课题单/002第k个数.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+const int N = 1e6 + 10;
+
+void quick_sort(int q[], int l, int r) {
+    if (l >= r) return;
+    int i = l - 1, j = r + 1, x = q[(l + r) >> 1];
+    while (i < j) {
+        do i++;
+        while (q[i] < x);
+        do j--;
+        while (q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+    quick_sort(q, l, j);
+    quick_sort(q, j + 1, r);
+}
+int n, k, q[N];
+int main() {
+    scanf("%d %d", &n, &k);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &q[i]);
+    }
+    quick_sort(q, 0, n - 1);
+    printf("%d", q[k - 1]);
+    return 0;
+}
---
+++ ACwing/算法基础课题单/003归并排序.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+const int N = 1e6 + 10;
+
+int n, q[N], tmp[N];
+
+void merge_sort(int q[], int l, int r) {
+    if (l >= r) return;
+    int mid = (l + r) >> 1;
+    merge_sort(q, l, mid);
+    merge_sort(q, mid + 1, r);
+
+    int k = 0, i = l, j = mid + 1;
+    while (i <= mid && j <= r)
+        if (q[i] < q[j])
+            tmp[k++] = q[i++];
+        else
+            tmp[k++] = q[j++];
+    while (i <= mid) tmp[k++] = q[i++];
+    while (j <= r) tmp[k++] = q[j++];
+    for (int i = l, j = 0; i <= r; i++, j++) q[i] = tmp[j];
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &q[i]);
+    merge_sort(q, 0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", q[i]);
+    return 0;
+}
