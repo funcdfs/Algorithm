4b2ad2d021632bf50e73ff1ec9009751a2ab20e5
fengwei2002 <konng_0120@qq.com>
2021-07-04 16:55:29 +0800

    高精度乘法，高精度除法，前缀和，差分

---
+++ ACwing/算法基础课题单/009高精度乘法.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> mul(vector<int> &A, int b) {
+    vector<int> C;
+    for (int i = 0, t = 0; i < A.size() || t; i++) {
+        if (i < A.size()) t += A[i] * b;
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+int main() {
+    string a;
+    int b;
+    cin >> a >> b;
+    vector<int> A;
+    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0');
+    auto C = mul(A, b);
+    for (int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
+    return 0;
+}
