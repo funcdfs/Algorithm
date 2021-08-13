5784c221a9d11dbac1bcddd7fa64bae807032fef
fengwei2002 <konng_0120@qq.com>
2021-08-13 23:30:54 +0800

    最长公共子序列 done

--- Leetcode/1143.最长公共子序列.cpp
+++ Leetcode/1143.最长公共子序列.cpp
@@ -4,32 +4,23 @@
  * [1143] 最长公共子序列
  */
 
-// @lc code=start
 #include <iostream>
 #include <vector>
 using namespace std;
+// date: 2021-08-13
+// @lc code=start
+
 class Solution {
    public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length(), n = text2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        // 定义:s1[0..i-1] 和 s2[0..j-1] 的 lcs 长度为 dp[i][j]
-        // 目标:s1[0..m-1] 和 s2[0..n-1] 的 lcs 长度，即 dp[m][n]
-        // base case: dp[0][..] = dp[..][0] = 0
-
-        for (int i = 1; i <= m; i++) {
+        vector<vector<int>> f(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++) {
-                // 现在 i 和 j 从 1 开始，所以要减一
-                if (text1.at(i - 1) == text2.at(j - 1)) {
-                    // s1[i-1] 和 s2[j-1] 必然在 lcs 中
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // s1[i-1] 和 s2[j-1] 至少有一个不在 lcs 中
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-                }
+                f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+                if (text1[i - 1] == text2[j - 1]) f[i][j] = f[i - 1][j - 1] + 1;
             }
-        }
-        return dp[m][n];
+        return f[m][n];
     }
 };
 // @lc code=end
