9e78e13744a2265ed3425ce8f7e78fba3b35b3dc
fengwei2002 <konng_0120@qq.com>
2021-08-18 23:53:12 +0800

    爬楼梯进阶

--- Leetcode/746.使用最小花费爬楼梯.cpp
+++ Leetcode/746.使用最小花费爬楼梯.cpp
@@ -8,31 +8,28 @@
 #include <vector>
 
 using namespace std;
-/*2021-08-09-09-47*/
-// ? 思路解析：
-/*
- *
- *
- *
- *
- ! --难点：
- ! --
- ! --
- */
+// date: 2021-08-17
+// ? 思路解析：题目描述不太行
 
 // @lc code=start
 // 版本一
 class Solution {
-   public:
+public:
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size());
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for (int i = 2; i < cost.size(); i++) {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        // 0 1 2 3 4 5 ... i - 1, i, 0;
+        int n = cost.size();
+        // f[i] 代表到达 i 阶时的花费数
+        // min 最低花费
+        // f[n] = f[n - 1] + cost[n]  
+        //      = f[n - 2] + cost[n];
+        vector<int> f(n + 10, 0);
+        f[0] = cost[0], f[1] = cost[1]; 
+        // 你可以从下标为 0 或者 下标为 1 作为初始阶梯
+        for (int i = 2; i < n; i++) {
+            f[i] = min(f[i - 1] + cost[i], f[i - 2] + cost[i]);
         }
-        // 注意最后一步可以理解为不用花费，所以取倒数第一步，第二步的最少值
-        return min(dp[cost.size() - 1], dp[cost.size() - 2]);
+        return min(f[n - 2], f[n - 1]);
+        //楼顶 0 的来源是，n - 1 和 n - 2 的最小值
     }
 };
 
