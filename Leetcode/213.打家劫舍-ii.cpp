653342c5333f49ffbec81c0ca0957a93ba6e8edc
fengwei2002 <konng_0120@qq.com>
2021-08-19 17:17:48 +0800

    动态规划 * 2

--- Leetcode/213.打家劫舍-ii.cpp
+++ Leetcode/213.打家劫舍-ii.cpp
@@ -11,39 +11,35 @@ using namespace std;
 /*2021-08-18-10-09*/
 // ? 思路解析：
 /*
- *
+ * 可以用状态机模型进行分析
  *
  !
+ 其实就是把环拆成两个队列，一个是从0到n-1，另一个是从1到n，然后返回两个结果最大的。
  !
  */
 
 // @lc code=start
 class Solution {
    public:
-    int rob(vector<int>& nums) {
-        // 相比于之前的问题，这里的房屋连成了一个环
-        // 可以将数组前面放入一个最后一个元素，后面在放入一个第一个元素
-        // f[i] 代表到达 第 i 个房屋的时候，可以拿到的最大金额
-        // max
-        // f[i] = f[i - 1]
-        //      = f[i - 2] + w[i]
-        // 由于两次计算了 头或者 尾 的最大值，结果再减去一个对应的值即可
-        int n = nums.size() - 1;
-        vector<int> f(n + 1, 0);
-        if (nums.size() > 1) {
-            f[0] = max(nums[n], nums[0]);
-            f[1] = max(f[0], nums[n] + nums[1]);
-            for (int i = 2; i <= n; i++) {
-                f[i] = max(f[i - 1], f[i - 2] + nums[i]);
-            }
-            f[n] -= f[0];
-            for (int i = 0; i < f.size(); i++) {
-                cout << f[i] << ' ';
-            }
-        } else {
-            f[0] = nums[0];
+    int robRange(vector<int>& nums, int start, int end) {
+        int first = nums[start], second = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
         }
-        return f[n];
+        return second;
+    }
+
+    int rob(vector<int>& nums) {
+        int length = nums.size();
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(robRange(nums, 0, length - 2),
+                   robRange(nums, 1, length - 1));
     }
 };
 // @lc code=end
