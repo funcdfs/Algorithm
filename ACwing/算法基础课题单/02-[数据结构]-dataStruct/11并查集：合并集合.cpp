fc4cf222010d13a4ee906cf4723701067786bcbe
fengwei2002 <konng0120@gmail.com>
2022-01-21 17:23:48 +0800

    use wsl edit this project

--- ACwing/算法基础课题单/02-[数据结构]-dataStruct/11并查集：合并集合.cpp
+++ ACwing/算法基础课题单/02-[数据结构]-dataStruct/11并查集：合并集合.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 using namespace std;
 
-const int N = 100010;
+const int N = 1e5 + 10;
 
-int p[N]; // 存储每个元素的 father 是谁
+int p[N];
 
-int find(int x) { // 返回 x 的祖宗节点 + 路径压缩
+int find(int x) {
     if (p[x] != x) p[x] = find(p[x]);
-    // 将路径上的所有点的父节点都变为 x 的父节点，搜索一遍，之后的操作都是 O(1)）
     return p[x];
 }
 
 int main() {
     int n, m;
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++) p[i] = i; 
-    // 初始化，所有节点的 p 值
-
+    cin >> n >> m;
+    
+    for (int i = 1; i <= n; i++) p[i] = i;
+    // 初始状态下每个元素单独存在于一个集合中
+    
     while (m--) {
-        char op[2];
+        string op;
         int a, b;
-        scanf("%s%d%d", op, &a, &b);
-        if (*op == 'M')
-            p[find(a)] = find(b); // 将 a 和 b 所在的两个集合进行合并
-        else {
-            if (find(a) == find(b)) // 询问编号为 a 的点和编号为 b 的点是否在同一个集合中
-                puts("Yes");
-            else
-                puts("No");
+        cin >> op >> a >> b;
+        if (op == "M") {
+            p[find(a)] = find(b);
+            // 合并两个集合
+        } else {
+            if (find(a) == find(b)) {
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }
+            // 查询两个集合是否在一个集合中
         }
     }
-
     return 0;
 }
-
-// https://www.acwing.com/activity/content/code/content/1463125/
---
+++ Leetcode/209.长度最小的子数组.go
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=golang
+ *
+ * [209] 长度最小的子数组
+ *
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (46.75%)
+ * Likes:    727
+ * Dislikes: 0
+ * Total Accepted:    172.8K
+ * Total Submissions: 369.5K
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ *
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr]
+ * ，并返回其长度。如果不,存在符合条件的子数组，返回 0 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 1
+ * 1
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ *
+ *
+ */
+package leetcode
+
+/* --- 2022-01-20-21-21 --- */
+
+// https://github.com/fengwei2002/Algorithm
+
+// solution link:
+// https://leetcode-cn.com/problems/minimum-size-subarray-sum/solution/lc209-fengwei2002-by-kycu-mfnn/
+
+// @lc code=start
+func minSubArrayLen(target int, nums []int) int {
+	// 找出数组中和大于等于 target 的长度最小的子数组
+	ans := (int)(1e9)
+	for i, j, sum := 0, 0, 0; i < len(nums); i++ {
+		sum += nums[i]
+		for sum-nums[j] >= target {
+			sum -= nums[j]
+			j++
+		}
+		if sum >= target {
+			ans = min(ans, i-j+1)
+		}
+	}
+	if ans == (int)(1e9) {
+		ans = 0
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
