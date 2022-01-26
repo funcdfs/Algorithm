6ddc93bfb965f4c91e8ec9c6de9b770baa490495
fengwei2002 <konng0120@gmail.com>
2022-01-26 12:12:47 +0800

    use cf tools

---
+++ Codeforces/template/main.cpp
@@ -0,0 +1,59 @@
+/*
+ @ $%U%$
+ @ $%Y%$-$%M%$-$%D%$-$%h%$-$%m%$-$%s%$
+ @ https://github.com/fengwei2002/Algorithm
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define INF 0x3f3f3f3f
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
