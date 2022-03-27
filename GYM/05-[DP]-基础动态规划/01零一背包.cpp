53a01a8b75d1de0b4c36d09b2b1753d57f095647
fengwei2002 <konng0120@gmail.com>
2022-03-27 21:07:34 +0800

    01背包和完全背包

--- GYM/05-[DP]-基础动态规划/01零一背包.cpp
+++ GYM/05-[DP]-基础动态规划/01零一背包.cpp
@@ -9,6 +9,7 @@ int n, m;        // 表示所有物品的个数
 int v[N], w[N];  // v 表示体积，w 代表价值
 int f[N];        // f[i][j] 代表状态
 
+// 魔法：
 int main() {
     cin >> n >> m;  // 读入物品数量背包容量
     for (int i = 1; i <= n; i++) {
@@ -17,7 +18,7 @@ int main() {
 
     for (int i = 0; i <= n; i++) {
         for (int j = m; j >= v[i]; j--) {
-            // i 正序遍历， j 逆序遍历
+            // i 正序遍历， j 逆序遍历 
             f[j] = max(f[j], f[j - v[i] + w[i]]);
         }
     }
@@ -25,3 +26,37 @@ int main() {
     return 0;
 }
 
+// 暴力解法：
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i]; 
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            f[i][j] = f[i - 1][j];  // 不选 i
+            if (j >= v[i]) f[i][j] = max(f[i][j], f[i - 1][j - v[i]] + w[i]); // 选 i 
+        }
+    }
+    
+    cout << f[n][m] << endl; 
+    
+    return 0; 
+}
+
+// 滚动数组：
+int main() {
+    int n, m;
+    cin >> n >> m; 
+    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i]; 
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            f[i & 1][j] = f[(i - 1) & 1][j]; 
+            if (j >= v[i]) f[i & 1][j] = max(f[i & 1][j], f[(i - 1) & 1][j - v[i]] + w[i]); 
+        }
+    }
+    
+    cout << f[n & 1][m] << endl; 
+    
+    return 0; 
+}
--- GYM/05-[DP]-基础动态规划/02完全背包问题.cpp
+++ GYM/05-[DP]-基础动态规划/02完全背包问题.cpp
@@ -9,17 +9,56 @@ const int N = 1010;
 
 int n, m;
 int v[N], w[N];
-int f[N];
+int f[N][N];
 
+// 暴力解法 
 int main() {
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i];
+    cin >> n >> m; 
+    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i]; 
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            f[i][j] = f[i - 1][j]; 
+            for (int k = 1; j >= v[i] * k; k++) {
+                f[i][j] = max(f[i][j], f[i - 1][j - v[i] * k] + w[i] * k); 
+            }
+        }
+    }
+    
+    cout << f[n][m] << endl; 
+    
+    return 0; 
+}
 
-    for (int i = 1; i <= n; i++)
-        for (int j = v[i]; j <= m; j++) 
-            f[j] = max(f[j], f[j - v[i]] + w[i]);
-    // i 正序遍历， j 正序遍历，
-    cout << f[m] << endl;
+// 时间复杂度 O(n^2) 
+// f[i][j] = f[i - 1][j] + f[i - 1][j - v[i]*1] + w[i]*1
+// f[i][j - v[i]] = f[i - 1][j - v[i]*1] ... 
+// f[i][j] = f[i - 1][j] , (f[i][j - v[i]] + w[i])
+int main() {
+    cin >> n >> m; 
+    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i]; 
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            f[i][j] = f[i - 1][j]; 
+            if (j >= v[i]) f[i][j] = max(f[i][j], f[i][j - v[i]] + w[i]); 
+        }
+    }
+}
 
-    return 0;
+// 再用一个滚动数组 
+int main() {
+    cin >> n >> m; 
+    for (int i = 1; i <= n; i++) cin >> v[i] >> w[i]; 
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            f[i & 1][j] = f[(i - 1) & 1][j]; 
+            if (j >= v[i]) f[i & 1][j] = max(f[i & 1][j], f[i & 1][j - v[i]] + w[i]); 
+        }
+    }
+    
+    cout << f[n & 1][m] << endl; 
+    
+    return 0; 
 }
