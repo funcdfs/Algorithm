a1b8ba6f49ef7f3a9996f0136245d806a4341446
fengwei2002 <konng0120@gmail.com>
2022-07-23 12:10:57 +0800

    最长上升子序列问题

--- GYM/05-DP/01-数字三角形模型/1027方格取数.cpp
+++ GYM/05-DP/01-数字三角形模型/1027方格取数.cpp
@@ -1,9 +1,17 @@
+// Problem: 方格取数
+// Contest: AcWing
+// URL: https://www.acwing.com/problem/content/description/1029/
+// date: 2022-07-22 11:42:05
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+
+
 /* https://github.com/fengwei2002/algorithm */
 // for debug:
 // #define d(a)  // cout << #a << " = " << a << ' ';
 // #define dline // cout << " ----- " << endl;
 // #define dendl // cout << " | \n";
-#define d(a) cout << #a << " = " << a << ' ';
+#define d(a)  cout << #a << " = " << a << ' ';
 #define dline cout << " ----- " << endl;
 #define dendl cout << " | \n";
 
@@ -20,43 +28,46 @@
 
 using namespace std;
 
-const int N = 15;
-int w[N][N];
-int f[N + N][N][N];
+const int N = 15; 
+int g[N][N]; 
+int f[N+N][N][N]; 
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-	
-	int n = 0; 
-	cin >> n; 
-
-    int a, b, c;
+    
+    int n = 0; 
+    cin >> n; 
+    int a, b, c; 
     while (cin >> a >> b >> c, a || b || c) {
-        w[a][b] = c;
+    	g[a][b] = c; 
     }
-
+    
     for (int k = 2; k <= n + n; k++) {
-        for (int i1 = 1; i1 <= n; i1++) {
-            for (int i2 = 1; i2 <= n; i2++) {
-                int j1 = k - i1;
-                int j2 = k - i2;
-                if (j1 >= 1 && j1 <= n && j2 >= 1 && j2 <= n) {
-                    int t = w[i1][j1];
-                    if (i1 != i2) {
-                        t += w[i2][j2];
-                    }
-
-                    int &x = f[k][i1][i2];
-                    x = max(x, f[k - 1][i1 - 1][i2 - 1] + t);
-                    x = max(x, f[k - 1][i1][i2 - 1] + t);
-                    x = max(x, f[k - 1][i1 - 1][i2] + t);
-                    x = max(x, f[k - 1][i1][i2] + t);
-                }
-            }
-        }
+    	for (int i1 = 1; i1 <= n; i1++) {
+    		for (int i2 = 1; i2 <= n; i2++) {
+    			int j1 = k - i1; 
+    			int j2 = k - i2; 
+    			
+    			if (j1 >= 1 && j1 <= n && j2 >= 1 && j2 <= n) {
+    				int t = g[i1][j1]; 
+    				if (j1 != j2) {
+    					t += g[i2][j2]; 
+    				} 
+    				
+    				int &x = f[k][i1][i2]; 
+    				x = max(x, f[k - 1][i1 - 1][i2 - 1]); 
+    				x = max(x, f[k - 1][i1][i2 - 1]); 
+    				x = max(x, f[k - 1][i1 - 1][i2]); 
+    				x = max(x, f[k - 1][i1][i2]); 
+    				
+    				x += t; 
+    			}
+    		}
+    	}
     }
-
-	cout << f[n + n][n][n]; 
+    
+    cout << f[n+n][n][n] << endl; 
+    
     return 0;
 }
---
+++ GYM/05-DP/02-最长上升子序列问题/02-登山.cpp
@@ -0,0 +1,76 @@
+// Problem: 登山
+// Contest: AcWing
+// URL: https://www.acwing.com/problem/content/1016/
+// date: 2022-07-22 11:52:59
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+
+
+/* https://github.com/fengwei2002/algorithm */
+// for debug:
+// #define d(a)  // cout << #a << " = " << a << ' ';
+// #define dline // cout << " ----- " << endl;
+// #define dendl // cout << " | \n";
+#define d(a)  cout << #a << " = " << a << ' ';
+#define dline cout << " ----- " << endl;
+#define dendl cout << " | \n";
+
+#define INF 0x3f3f3f3f
+#define LL long long
+#define PII pair<int, int>
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n = 0; 
+    cin >> n; 
+    
+    vector<int> h(n, 0); 
+    
+    for (int i = 0; i < n; i++) {
+    	cin >> h[i]; 
+    }
+    
+	// 然后计算 h[i] 的左侧可以得到的最大的景点的数量
+	// 再预处理 h[i] 的右侧可以得到的最大的景点的数量
+	// 最后遍历求和 同时维护最大值 
+	
+	vector<int> left(n, 0); 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (h[j] < h[i]) {
+				left[i] = max(left[i], left[j] + 1); 
+			}
+		}
+	}
+	
+	vector<int> right(n, 0); 
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = n - 1; j >= i; j--) {
+			if (h[j] < h[i]) {
+				right[i] = max(right[i], right[j] + 1); 
+			}
+		}
+	}
+    
+    int ans = 0; 
+    for (int i = 0; i < n; i++) {
+    	ans = max(ans, left[i] + right[i] + 1); 
+    } 
+    
+    cout << ans << endl; 
+    
+    return 0;
+}
