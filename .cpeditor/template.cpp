5cd09cb802708ae6cac4186ef1b499515171e708
fengwei2002 <konng0120@gmail.com>
2022-08-03 14:42:49 +0800

    update template file and config

---
+++ .cpeditor/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 1
+#endif
+/*-----------------------*/
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+
+    return 0;
+}
