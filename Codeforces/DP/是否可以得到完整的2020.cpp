95783159246c23658a272170e8540941467642fc
fengwei2002 <konng0120@gmail.com>
2022-08-04 17:07:38 +0800

    dp training

---
+++ Codeforces/DP/是否可以得到完整的2020.cpp
@@ -0,0 +1,54 @@
+// URL: https://codeforces.com/problemset/problem/1462/B
+// NAME: B. Last Year's Substring, Codeforces - Codeforces Round #690 (Div. 3)
+// LIMIT: 256 MB, 2000 ms
+// DATE: 2022-08-04 15:15:29
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*-----------------------*/
+
+bool solve(string& s) {
+	int n = s.size(); 
+	for (int i = 0; i <= 4; i++) {
+		if (s.substr(0, i) + s.substr(n - 4 + i, 4 - i) == "2020") {
+			return true; 
+		}
+	} 
+	return false; 
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int tt = 0; 
+    cin >> tt; 
+    
+    while (tt--) {
+    	int length = 0; 
+    	cin >> length; 
+    	string s = ""; 
+    	cin >> s; 
+    	if (solve(s)) {
+    		cout << "YES" << endl; 
+    	} else {
+    		cout << "NO" << endl; 
+    	}
+    }
+
+    return 0;
+}
+
+
+/*
+
+是否可以得到完整的2020
+
+*/
---
+++ Codeforces/DP/计算菱形中格子的数量.cpp
@@ -0,0 +1,50 @@
+// URL: https://codeforces.com/problemset/problem/1180/A
+// NAME: A. Alex and a Rhombus, Codeforces - Codeforces Round #569 (Div. 2)
+// LIMIT: 256 MB, 1000 ms
+// DATE: 2022-08-04 16:34:13
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*-----------------------*/
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n = 0; 
+    cin >> n; 
+    
+    unordered_map<int, int> f; 
+    
+    f[1] = 1; 
+    f[2] = 5; 
+    
+    for (int i = 3; i <= n; i++) {
+    	f[i] = f[i-1] + 4 + (i - 2) * 2 * 2;  
+    	// 上下两个部分的边边都多了两块，所以需要在 (i - 2) * 2 的基础上再乘 2 
+    }
+
+	cout << f[n] << endl; 
+
+    return 0;
+}
+
+
+/*
+
+计算菱形中格子的数量
+
+1 1 
+2 1+2+2 
+3 5+2+2 +2+2
+
+*/
