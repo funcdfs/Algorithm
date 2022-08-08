a4a74f0619a1838d5ac24bc95556a1dece62b9c0
fengwei2002 <konng0120@gmail.com>
2022-08-08 15:08:11 +0800

    update codeforces template and cf-tools

---
+++ .cf/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*-----------------------*/
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+
+    return 0;
+}
+
+/*
+
+
+
+*/
--- Codeforces/template/main.cpp
+++ Codeforces/template/main.cpp
@@ -1,40 +1,30 @@
-/*
- @ $%U%$
- @ $%Y%$-$%M%$-$%D%$-$%h%$-$%m%$-$%s%$
- @ https://github.com/fengwei2002/Algorithm
-*/
-
-// #define d(a)  // https://github.com/fengwei2002/algorithm
-// #define dline // konng0120@gmail.com
-// #define dendl // 
-
-#define d(a)  cout << #a << " = " << a << ' ';
-#define dline cout << " ----- " << endl;
-#define dendl cout << " | \n";
-
-#define LL  long long
-#define ULL unsigned long long
-#define PII pair<int, int>
-#define INF 0x3f3f3f3f
-
-#include <algorithm>
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <functional>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*-----------------------*/
 
 
 
 int main() {
-    ios::sync_with_stdio(0);
+    ios::sync_with_stdio(false);
     cin.tie(0);
     
     
-    
+
     return 0;
 }
+
+/*
+
+
+
+*/
 
 
 
