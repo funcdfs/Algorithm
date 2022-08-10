cf0dbe96707b94b7b4945c2246a9090cff9a9a7f
fengwei2002 <konng0120@gmail.com>
2022-08-10 14:25:00 +0800

    800-rating dp problems

---
+++ Codeforces/traning-DP/1539B前缀和.cpp
@@ -0,0 +1,47 @@
+// URL: https://codeforces.com/contest/1539/problem/B
+// NAME: B. Love Song, Codeforces - Codeforces Round #727 (Div. 2)
+// LIMIT: 256 MB, 2000 ms
+// DATE: 2022-08-09 23:11:37
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*-----------------------*/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n = 0, q = 0; 
+    cin >> n >> q; 
+    
+    string s; 
+    cin >> s; 
+    
+    vector<int> sum(n + 1, 0); 
+    for (int i = 1; i <= n; i++) {
+    	sum[i] = sum[i - 1] + s[i - 1] - 'a' + 1;
+    }
+    
+    while (q--) {
+    	int l = 0, r = 0; 
+    	cin >> l >> r;  
+    	
+    	cout << sum[r] - sum[l - 1] << endl; 
+    }
+
+    return 0;
+}
+
+
+/*
+
+
+
+*/
