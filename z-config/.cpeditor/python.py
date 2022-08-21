cb596ae931ba55a98f70f66b740ffb7b63b17cc6
fengwei2002 <konng0120@gmail.com>
2022-08-21 15:16:40 +0800

    update python config

---
+++ z-config/.cpeditor/python.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    n = input() 
+    a = list(map(int, input().split()))
+    
+    print(a, n)
+
+
+if __name__ == "__main__":
+    main() 
---
+++ z-config/.cpeditor/template.py
@@ -0,0 +1,9 @@
+
+
+
+def main():
+    
+
+
+if __name__ == "__main__": 
+    main() 
