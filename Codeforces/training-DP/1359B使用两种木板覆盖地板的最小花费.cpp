8ec33c16eddd3acbf19b6328d8af8f921ae36132
fengwei2002 <konng0120@gmail.com>
2022-08-22 12:41:59 +0800

    dp * 5

---
+++ Codeforces/training-DP/1359B使用两种木板覆盖地板的最小花费.cpp
@@ -0,0 +1,94 @@
+// URL: https://codeforces.com/contest/1359/problem/B
+// NAME: B. New Theatre Square, Codeforces - Educational Codeforces Round 88 (Rated for Div. 2)
+// LIMIT: 256 MB, 2000 ms, DATE: 2022-08-21 18:07:50
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+/*----------------------------------*/
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*----------------------------------*/
+
+int calRowValue(vector<char>& a, int x, int y) {
+    /*
+    if a[i] == '*': 
+        f[i] = f[i - 1] 
+    if a[i] == '.' && a[i - 1] == '*': 
+        f[i] = f[i - 1] + x
+    if a[i] == '.' && a[i - 1] == '.': 
+        f[i] = f[i - 1] + x || f[i - 2] + y
+    */ 
+    vector<int> f(a.size(), 0); 
+    
+    f[0] = a[0] == '.' ? x : 0; 
+    if (a[0] == '.' && a[1] == '.') {
+        f[1] = min(f[0] + x, y); 
+    } else if (a[0] == '*' && a[1] == '.') {
+        f[1] = f[0] + x; 
+    } else {
+        f[1] = f[0]; 
+    }
+    
+    for (int i = 2; i < a.size(); i++) {
+        if (a[i] == '*') {
+            f[i] = f[i - 1]; 
+        } else if (a[i] == '.' && a[i - 1] == '*') {
+            f[i] = f[i - 1] + x;  
+        } else if (a[i] == '.' && a[i - 1] == '.') {
+            f[i] = min(f[i - 1] + x, f[i - 2] + y); 
+        }
+    } 
+    debug(f); 
+    return f[a.size() - 1]; 
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    
+    int tt = 0;
+    cin >> tt; 
+    while (tt--) {
+        int n, m, x, y; 
+        cin >> n >> m >> x >> y; 
+        
+        vector<vector<char>> g(n, vector<char>(m, ' ')); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> g[i][j]; 
+            }
+        } 
+        
+        // covered all . tiles 
+        // use 1*1 and 1*2 
+        
+        int ans = 0; 
+        
+        for (int i = 0; i < n; i++) {
+            ans += calRowValue(g[i], x, y); 
+        } 
+        
+        cout << ans << "\n"; 
+    }
+    
+    return 0;
+}
+
+
+/*
+
+1359B使用两种木板覆盖地板的最小花费
+
+思路: 
+
+先使用 1*2 的将所有的空地都尽可能地塞满，然后使用 1*1 的木板来填补空隙
+
+由于不能旋转这些木板，所以可以一行一行的来看 
+
+
+线性DP
+*/
---
+++ Codeforces/training-DP/1553C根据预测判断罚球的最早结束时间.cpp
@@ -0,0 +1,89 @@
+// URL: https://codeforces.com/contest/1553/problem/C
+// NAME: C. Penalty, Codeforces - Harbour.Space Scholarship Contest 2021-2022
+// (open for everyone, rated, Div. 1 + Div. 2) LIMIT: 256 MB, 3000 ms DATE:
+// 2022-08-19 10:24:25
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 4
+#define dline() 2
+#endif
+/*-----------------------------------*/
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int tt = 0;
+    cin >> tt;
+    while (tt--) {
+        string s;
+        cin >> s;
+
+        int ans = 9;
+        {
+            int cnt0 = 0, cnt1 = 0;
+            for (int i = 0; i < 10; i++) {
+                // 假设此时的第一个队伍是赢家，所以 ? 要归顺于第一个玩家
+                if (i % 2 == 0) {
+                    cnt0 += s[i] != '0';
+                } else {
+                    cnt1 += s[i] == '1';
+                }
+                // 如果将剩下的所有的 cnt1 += cnt1 < cnt0 更新 ans
+                if (cnt0 > cnt1 + (9 - i + 1) / 2) ans = min(ans, i);
+                // 如果将剩下的所有的 cnt0 + 现在的，都不能超过 cnt1 更新 ans
+                if (cnt1 > cnt0 + (8 - i + 1) / 2) ans = min(ans, i);
+            }
+        }
+        {
+            int cnt0 = 0, cnt1 = 0;
+            for (int i = 0; i < 10; i++) {
+                if (i % 2 == 0) {
+                    cnt0 += s[i] == '1';
+                } else {
+                    cnt1 += s[i] != '0';
+                }
+                if (cnt0 > cnt1 + (10 - i) / 2) ans = min(ans, i);
+                if (cnt1 > cnt0 + (9 - i) / 2) ans = min(ans, i);
+            }
+        }
+
+        cout << ans + 1 << "\n"; // ans 是具体的下标
+    }
+
+    return 0;
+}
+
+/*
+
+1553C根据预测判断罚球的最早结束时间
+
+       1
+       |
+       |
+1 3 5 7  9
+2 4 6 8 10
+     |
+     |
+     3
+
+由于第一个队伍不能达到 3 个球了，所以罚球结束
+
+例如 0100000000
+
+在第九次罚球之后，第一个队伍没有机会得到新的一分了，所以裁判就会在第 9
+个位置结束
+
+根据预测的字符串，输出最小的结束位置
+
+
+
+
+
+
+*/
