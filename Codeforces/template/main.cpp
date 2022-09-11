3f30285e8ca5fa38b30371b32bfe86d07f48f03d
fengwei2002 <konng0120@gmail.com>
2022-09-11 23:14:14 +0800

    20220911-231414

--- Codeforces/template/main.cpp
+++ Codeforces/template/main.cpp
@@ -1,61 +1,27 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/*----------------------------------*/
 #ifdef github_fengwei2002
 #include "algo/debug.h"
 #else
 #define debug(...) 42
 #endif
-/*-----------------------*/
+/*----------------------------------*/
 
 
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr), ios::sync_with_stdio(false);
     
     
-
+    
     return 0;
 }
+
 
 /*
 
 
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
