21bb6d968b258e0013984febb8bf313bd93d0aa0
fengwei2002 <konng0120@gmail.com>
2022-09-13 23:05:52 +0800

    20220913-230552

---
+++ Atcoder/Boot camp for Beginners EASY100/abc139_b.cpp
@@ -0,0 +1,43 @@
+// LINK: https://atcoder.jp/contests/abc139/tasks/abc139_b
+// NAME: B - Power Socket, AtCoder - AtCoder Beginner Contest 139
+// DATE: 2022.09.13 21:59:57, LIMIT: 1024 MB, 2000 ms
+
+#include <bits/stdc++.h>
+
+using namespace std;
+/*----------------------------------*/
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*----------------------------------*/
+
+
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+    int a = 0, b = 0; 
+    cin >> a >> b; 
+    int ans = 0; 
+    int left = 1; 
+    while (left < b) {
+        left--; // use a socket 
+        left += a; // use this socket to connect a another socket 
+        ans++; // the extra socket num++ 
+    }
+    
+    cout << ans << '\n'; 
+    
+    return 0;
+}
+
+
+/*
+
+模拟插线板扩展问题
+
+![](https://raw.githubusercontent.com/psychonaut1f/a/main/img/20220913221107.png)
+
+*/
---
+++ Atcoder/Boot camp for Beginners EASY100/codefestival_2016_qualB_b.cpp
@@ -0,0 +1,67 @@
+// LINK: https://atcoder.jp/contests/code-festival-2016-qualb/tasks/codefestival_2016_qualB_b
+// NAME: B - Qualification simulator, AtCoder - CODE FESTIVAL 2016 qual B
+// DATE: 2022.09.13 22:38:38, LIMIT: 256 MB, 2000 ms
+
+#include <bits/stdc++.h>
+
+using namespace std;
+/*----------------------------------*/
+#ifdef github_fengwei2002
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+/*----------------------------------*/
+
+
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+    int n, a, b; 
+    cin >> n >> a >> b; 
+    string s = ""; 
+    cin >> s; 
+    
+    vector<int> pos(0, 0); 
+    unordered_map<int, int> hs; 
+    for (int i = 0; i < (int)s.size(); i++) {
+        if (s[i] == 'b') {
+            pos.push_back(i); 
+            hs[i] = pos.size(); 
+        }
+    }
+    
+    int passed = 0; 
+    
+    for (int i = 0; i < (int)s.size(); i++) {
+        bool ok = false; 
+        if (s[i] == 'a' && passed < a + b) {
+            // jp student 
+            cout << "Yes\n"; 
+            ok = true;
+            passed++; 
+        } else if (s[i] == 'b' && passed < a + b) {
+            // oversea student
+            if (hs[i] <= b) {
+                cout << "Yes\n"; 
+                ok = true; 
+                passed++; 
+            }
+        } 
+        if (ok == false) {
+            cout << "No\n"; 
+        }
+    }
+    
+    return 0;
+}
+
+
+/*
+
+日本学生和留学生
+
+https://atcoder.jp/contests/code-festival-2016-qualb/submissions/34841418
+
+*/
