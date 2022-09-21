5785a2d46cd4a1be347c3b0e4a4df8b2c3095f26
fengwei2002 <konng0120@gmail.com>
2022-09-21 09:36:19 +0800

    20220921-09:36:19

---
+++ Spoj/TEST.cpp
@@ -0,0 +1,40 @@
+// LINK: https://www.spoj.com/problems/TEST/
+// NAME: Life, the Universe, and Everything, SPOJ - Classical
+// DATE: 2022.09.20 21:53:43, LIMIT: 1536 MB, 10000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+/*----------------------------------*/
+#ifdef github_fengwei2002
+#include "algo/dbg.h"
+#else
+#define dbg(...) 42
+#endif
+/*----------------------------------*/
+
+
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+    int x = 0; 
+    while (cin >> x, x != 42) {
+        cout << x << '\n'; 
+    }
+    
+    return 0;
+}
+
+
+/*
+
+spoj 
+
+the origin of 42 code 
+
+TEST - Life, the Universe, and Everything
+
+https://www.spoj.com/problems/TEST/
+
+
+*/
