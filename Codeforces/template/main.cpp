72764b6fd6304e3d65b30cf0c57ebe1a4371d3dc
fengwei2002 <konng0120@gmail.com>
2022-11-14 13:28:33 +0800

    update cf template

--- Codeforces/template/main.cpp
+++ Codeforces/template/main.cpp
@@ -1,13 +1,16 @@
+/**
+ * author: $%U%$ https://github.com/fengwei2002
+ * time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-/*----------------------------------*/
+
 #ifdef LOCAL
 #include "algo/dbg.h"
 #else
 #define dbg(...) 42
 #endif
-/*----------------------------------*/
-
 
 
 int main() {
--- z-config/.cf/main.cpp
+++ z-config/.cf/main.cpp
@@ -1,21 +1,24 @@
+/**
+ * author: $%U%$ https://github.com/fengwei2002
+ * time: $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-/* ------------------------------------------ */
+
 #ifdef LOCAL
 #include "algo/dbg.h"
 #else
 #define dbg(...) 42
 #endif
-/* ------------------------------------------ */
-
 
 
 int main() {
-   cin.tie(nullptr), ios::sync_with_stdio(false);
-   
-   
-   
-   return 0;
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    
+    
+    
+    return 0;
 }
 
 
