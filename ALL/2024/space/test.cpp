1727fe506867a0b298280b85e67bb8af1a310ec1
funcdfs <funcdfs@gmail.com>
2024-05-14 22:25:09 +0800

    20240514-22:25:09

--- ALL/2024/space/test.cpp
+++ ALL/2024/space/test.cpp
@@ -1,22 +1,26 @@
+/**
+ * author: https://github.com/funcdfs
+ * created: 2024-05-14 22:22:48
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
-#ifdef LOCAL /* For Local Debugging Purposes */
+
+#ifdef LOCAL
 #include "algo/dbg.h"
 #else
 #define dbg(...) 1
-#endif
+#endif /* https://github.com/funcdfs/Algorithm/blob/main/AAA/dbg.h */
+
+
 
 auto main() -> int32_t {
     cin.tie(nullptr), ios::sync_with_stdio(false);
-
-string a(10, ' ');
-    cerr << std::format("{0}", a);
-    cerr << std::format("\n");
-    cerr << std::format("{0}", a | std::views::reverse);
-
+    
+    
+    
     return 0;
 }
 /*
 
 */
-
