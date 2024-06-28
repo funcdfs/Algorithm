6fe89e99492d0c6572a8d9005dce8c4b47dd455a
funcdfs <funcdfs@gmail.com>
2024-06-28 19:29:14 +0800

    20240628-19:29:14

--- misc/lang-golang/ttna.go
+++ misc/lang-golang/ttna.go
@@ -1,32 +1,19 @@
-// author:  https://github.com/funcdfs 
+// #region main
+package main; import ("bufio"; "fmt"; "os";); var _in, _out = new(bufio.Reader), new(bufio.Writer);
+func main() { _in = bufio.NewReader(os.Stdin); _out = bufio.NewWriter(os.Stdout); defer _out.Flush(); testCaseCnt := input[int](); for i := 0; i < testCaseCnt; i++ { solve(i + 1); } }
+func input[T any]() T { var value T; fmt.Fscan(_in, &value); return value }; func println[T any](x ...T) { print(" ", "\n", x...) };
+func inputSlice[T any](length int) []T { data := make([]T, length); for i := 0; i < length; i++ { data[i] = input[T](); }; return data; }
+func print[T any](sep, end string, x ...T) { for i := range x { fmt.Fprint(_out, x[i]); if i == len(x)-1 { fmt.Fprint(_out, end); } else { fmt.Fprint(_out, sep); } } }
+// #endregion main
 
-package main
+// ---------------------------------------------------------------
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+func solve(_case int) {
 
-func solve(_in *bufio.Reader, _out *bufio.Writer) {
-	n := 0
-	fmt.Fscan(_in, &n) 
-	a := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Fscan(_in, &a[i]) 
-	}
+	n := input[int]()
+	a := inputSlice[int](n) 
 	
-	$0
 	
 }
 
-func main() {
-	_in := bufio.NewReader(os.Stdin)
-	_out := bufio.NewWriter(os.Stdout)
-	defer _out.Flush()
-
-	var TT int
-	for fmt.Fscan(_in, &TT); TT > 0; TT-- {
-		solve(_in, _out)
-	}
-}
+// ---------------------------------------------------------------
--- misc/lang-python/cp.py
+++ misc/lang-python/cp.py
@@ -1,21 +1,21 @@
 import sys
 
 
-# region solve
-# ------------------------------------------------------------
-def solve() -> None:
-    
-
-# ------------------------------------------------------------
-# endregion solve
-
-
-# region main
+# region fastIO
 input_int = lambda: int(input_str())  # noqa: E731
 input_str = lambda: sys.stdin.readline().rstrip()  # noqa: E731
 input_multi_int = lambda: map(int, input_str().split())  # noqa: E731
 input_list = lambda: list(map(int, input_str().split()))  # noqa: E731
 dbg = lambda *args: sys.stderr.write(" ".join(map(str, args)) + "\n")  # noqa: E731
+# endregion fastIO
 
+# region main
 solve()
 # endregion main
+
+
+# ------------------------------------------------------------
+def solve() -> None:
+    
+
+# ------------------------------------------------------------
