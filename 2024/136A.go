f357fc1e11e2dfe5f5304b73807f938a0ec40bc7
funcdfs <funcdfs@gmail.com>
2024-09-14 21:25:04 +0800

    20240914-21:25:04

---
+++ 2024/136A.go
@@ -0,0 +1,34 @@
+// link: https://codeforces.com/contest/136/problem/A A. Presents
+// time: 2024/9/14 21:20:27 https://github.com/funcdfs
+
+// #region main
+package main; import ( "bufio"; "fmt"; "os"; ); var _in, _out = new(bufio.Reader), new(bufio.Writer)
+func _github_funcdfs[T any](sep, end string, arr ...T) { for idx := range arr { fmt.Fprint(_out, arr[idx]); if idx == len(arr)-1 { fmt.Fprint(_out, end); } else { fmt.Fprint(_out, sep) } } }
+func main() { _in = bufio.NewReader(os.Stdin); _out = bufio.NewWriter(os.Stdout); defer _out.Flush(); solve() }
+func input      [T any] ()           T { var value T; fmt.Fscan(_in, &value); return value }
+func inputSlice [T any] (size int) []T { data := make([]T, size); for idx := 0; idx < size; idx++ { data[idx] = input[T](); }; return data }
+func print      [T any] (arr ...T)     { _github_funcdfs("", "", arr...) }
+func println    [T any] (arr ...T)     { _github_funcdfs(" ", "\n", arr...) }
+// #endregion main
+
+
+// ----------------------------- /* Start of useful functions */ -----------------------------
+
+func solve() {
+
+	n := input[int]()
+	a := inputSlice[int](n) 
+	
+	hs := make(map[int]int)
+	for i := range a {
+		hs[a[i]] = i+1
+	}
+
+	for i := range a {
+		print(hs[hs[a[i]]])
+		print(" ")
+	}
+
+}
+
+// ----------------------------- /* End of useful functions */ -------------------------------
