9e8dc00b3b81468c0da939313afdd6f86028c57b
funcdfs <funcdfs@gmail.com>
2025-03-04 12:21:40 +0800

    upd

---
+++ 2025/1692B.cpp
@@ -0,0 +1,60 @@
+// link: https://codeforces.com/contest/1692/problem/B -> B. All Distinct
+// time: 2025/2/19 13:32:27 https://github.com/funcdfs
+
+#pragma region headers_and_definitions // clang-format off
+#include <bits/stdc++.h>
+using namespace std; /* golang types */
+/* int  int8  int16   int32   int64  */ using int8 = signed char; using int16 = short; using int32 = int; using int64 = long long;
+/* uint uint8 uint16  uint32  uint64 */ using uint8 = unsigned char; using uint16 = unsigned short; using uint32 = unsigned int; using uint64 = unsigned long long;
+/* byte rune  float32 float64        */ using byte = unsigned char; using rune = int; using float32 = float; using float64 = double;
+template <class T> istream &operator>> (istream &cin , vector<T>           &a)  { for (auto &x : a) { cin >> x; } return cin; }
+template <class T> ostream &operator<< (ostream &cout, vector<T>           &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << ' ' << a[i]; } return cout; }
+template <class T> istream &operator>> (istream &cin , valarray<T>         &a)  { for (auto &x : a) { cin >> x; } return cin; }
+template <class T> ostream &operator<< (ostream &cout, valarray<T>         &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << ' ' << a[i]; } return cout; }
+template <class T> ostream &operator<< (ostream &cout, vector<vector<T>>   &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << '\n' << a[i]; } return cout; }
+template <class T> ostream &operator<< (ostream &cout, vector<valarray<T>> &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << '\n' << a[i]; } return cout; }
+struct __init_io { __init_io() { std::cout << fixed << setprecision(       15   ); cin.tie(nullptr); cin.exceptions(ios::badbit | ios::failbit); ios::sync_with_stdio(false);  }} _github_funcdfs; // Global instance
+#define endl '\n'
+auto solve(int _case) -> void; int32 main() { int testCaseCnt = 0; cin >> testCaseCnt; for (int _case = 1; _case <= testCaseCnt; _case++) { solve(_case); } return 0; }
+#ifdef LOCAL
+#include <algo/dbg.h>
+#else
+#define dbg(...)      ;
+#define eprint(...)   ;
+#define eprintln(...) ;
+#endif
+template<class T1, class T2> bool Min(T1& a, const T2& b) { return b < a ? a = b, true : false; }
+template<class T1, class T2> bool Max(T1& a, const T2& b) { return a < b ? a = b, true : false; }
+#pragma endregion headers_and_definitions       // clang-format on
+
+// ----------------------------- /* Start of useful functions */ -----------------------------
+
+
+auto solve(int _case) -> void {
+
+   int n = 0;
+   cin >> n;
+   vector<int> a(n, 0);
+   cin >> a;
+
+   unordered_map<int, int> hs;
+   for (auto& x : a) {
+      hs[x]++;
+   }
+   int need = 0;
+   for (auto& [k, v] : hs) {
+      if (v > 1) {
+         need += v - 1;
+      }
+   }
+   if (need % 2 == 1) {
+      cout << n-  2*(need / 2 + 1) << endl;
+   } else {
+      cout << n- 2*(need / 2) << endl;
+   }
+
+
+   return;
+}
+
+// ----------------------------- /* End of useful functions */ -------------------------------
---
+++ 2025/1843A.cpp
@@ -0,0 +1,54 @@
+// link: https://codeforces.com/contest/1843/problem/A -> A. Sasha and Array Coloring
+// time: 2025/3/3 20:48:29 https://github.com/funcdfs
+
+#pragma region headers_and_definitions // clang-format off
+#include <bits/stdc++.h>
+using namespace std; /* golang types */
+/* int  int8  int16   int32   int64  */ using int8 = signed char; using int16 = short; using int32 = int; using int64 = long long;
+/* uint uint8 uint16  uint32  uint64 */ using uint8 = unsigned char; using uint16 = unsigned short; using uint32 = unsigned int; using uint64 = unsigned long long;
+/* byte rune  float32 float64        */ using byte = unsigned char; using rune = int; using float32 = float; using float64 = double;
+template <class T> istream &operator>> (istream &cin , vector<T>           &a)  { for (auto &x : a) { cin >> x; } return cin; }
+template <class T> ostream &operator<< (ostream &cout, vector<T>           &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << ' ' << a[i]; } return cout; }
+template <class T> istream &operator>> (istream &cin , valarray<T>         &a)  { for (auto &x : a) { cin >> x; } return cin; }
+template <class T> ostream &operator<< (ostream &cout, valarray<T>         &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << ' ' << a[i]; } return cout; }
+template <class T> ostream &operator<< (ostream &cout, vector<vector<T>>   &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << '\n' << a[i]; } return cout; }
+template <class T> ostream &operator<< (ostream &cout, vector<valarray<T>> &a)  { int n = int(a.size()); if (!n) { return cout; } cout << a[0]; for (int i = 1; i < n; i++) { cout << '\n' << a[i]; } return cout; }
+struct __init_io { __init_io() { std::cout << fixed << setprecision(       15   ); cin.tie(nullptr); cin.exceptions(ios::badbit | ios::failbit); ios::sync_with_stdio(false);  }} _github_funcdfs; // Global instance
+#define endl '\n'
+auto solve(int _case) -> void; int32 main() { int testCaseCnt = 0; cin >> testCaseCnt; for (int _case = 1; _case <= testCaseCnt; _case++) { solve(_case); } return 0; }
+#ifdef LOCAL
+#include <algo/dbg.h>
+#else
+#define dbg(...)      ;
+#define eprint(...)   ;
+#define eprintln(...) ;
+#endif
+template<class T1, class T2> bool Min(T1& a, const T2& b) { return b < a ? a = b, true : false; }
+template<class T1, class T2> bool Max(T1& a, const T2& b) { return a < b ? a = b, true : false; }
+#pragma endregion headers_and_definitions       // clang-format on
+
+// ----------------------------- /* Start of useful functions */ -----------------------------
+
+
+auto solve(int _case) -> void {
+
+   int n = 0;
+   cin >> n;
+   vector<int> a(n, 0);
+   cin >> a;
+
+   int ans = 0;
+   sort(a.begin(), a.end());
+   int left = 0, right = n-1;
+   while (left < right) {
+      ans += a[right] - a[left];
+      left += 1;
+      right -= 1;
+   }
+
+   cout << ans << endl;
+
+   return;
+}
+
+// ----------------------------- /* End of useful functions */ -------------------------------
